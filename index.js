af6f83c5d36576ff8c1eac87cb06c58822111ecf
Roderik van der Veer <roderik@settlemint.io>
2017-11-19 13:42:51 +0100

    fix: there is no ./lib/config file

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var pkg = require("./package.json");
 module.exports = {
   build: require("./lib/build"),
   create: require("./lib/create"),
-  config: require("./lib/config"),
   console: require("./lib/repl"),
   contracts: require("./lib/contracts"),
   init: require("./lib/init"),
